536f8bd1bae7570a11c692c6d8e90ba319ab8364
remi-mnr <naughty.remz@gmail.com>
2021-03-03 19:23:23 +0100

    Webdoc Page

--- src/components/webdoc.js
+++ src/components/webdoc.js
@@ -3,7 +3,7 @@ import sal from 'sal.js'
 import clsx from 'clsx';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronRight, faCalendarDay, faClock, faCalendarCheck } from '@fortawesome/free-solid-svg-icons'
 
 export default function Webdoc() {
     sal();
@@ -12,16 +12,20 @@ export default function Webdoc() {
 
             <div className={styles.gradientBlock}/>
 
-            <img className={styles.smallRound} src="/static/images/ski.png" alt=""/>
-            <img className={styles.bigRound} src="/static/images/ski.png" alt=""/>
+            <img className={styles.smallRound} src="/static/images/ski2.jpg" alt=""/>
+            <img className={styles.bigRound} src="/static/images/ski2.jpg" alt=""/>
 
-            <img className={styles.pc} src="/static/images/webdoc_pc.png" alt=""/>
+            <img className={styles.pc} src="/static/images/webdoc_pc-min.png" alt=""/>
             <img className={styles.phone} src="/static/images/webdoc_phone.png" alt=""/>
 
             <div className={styles.infos}>
 
                 <img data-sal="slide-up" data-sal-delay="200"
                 src="/static/images/coulissesLogo.png" alt=""/>
+
+                <div className={styles.dateContainer} data-sal="slide-up" data-sal-delay="500">
+                    <FontAwesomeIcon icon={faCalendarCheck} /><p>2018</p>
+                </div>
                 
                 <p data-sal="slide-down" data-sal-delay="400"
                 className={clsx(styles.subtitle, "swiper-no-swiping")}>Ce site est un <b>Web-Documentaire</b> sur les coulisses des sports extrêmes. Il propose une navigation originale et se veut hautement interactif.</p>
