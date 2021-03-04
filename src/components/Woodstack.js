239d62b44b369f373ebd70ec5553f2f31586bb67
remi-mnr <naughty.remz@gmail.com>
2021-03-04 19:54:18 +0100

    Placeholder Woodstack

---
+++ src/components/Woodstack.js
@@ -0,0 +1,11 @@
+import styles from "../../styles/Woodstack.module.scss"
+// import sal from 'sal.js'
+
+export default function Webdoc() {
+    // sal();
+    return (
+        <div className={styles.slide}>
+            
+        </div>
+    )
+}
